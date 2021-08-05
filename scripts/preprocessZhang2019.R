4b2f9717bc2e8734295a83e12b07ca7b0ebe990a
Wang, Yucheng <yw19282@essex.ac.uk>
2021-08-05 21:18:06 +0100

    fix bugs

--- scripts/preprocessZhang2019.R
+++ scripts/preprocessZhang2019.R
@@ -1,22 +1,26 @@
 ######## this script was modified from Qian Zhang's work(https://github.com/qzhang314/DNAm-based-age-predictor/blob/master/pred.R). 
 
 
-
 ## for each probe, change to missing value to the mean value across all individuals #############
 addna <- function(methy){
-    methy[is.na(methy)] <- mean(methy,na.rm=T)
-    methy[is.na(methy)] <- 0 ##wyc
+    mean_p <- mean(methy, na.rm=T)
+    if (is.na(mean_p)){
+        mean_p <- 0
+    }
+    methy[is.na(methy)] <- mean_p
     return(methy)
 }
 
 preprocessZhang2019 <- function(betas){
-    message("replace the NA with mean value for each probe ")
-    betas <- apply(betas, 2, function(x) addna(x))   
-
+    sample_id <- rownames(betas)
+    if(all(is.na(betas))){
+        message("Found NAs, try to replace the NA with mean value for each probe: ")
+        betas <- t(apply(betas, 1, addna))
+    }
     ### standardize the DNA methylation within each individual, remove the mean and divided by the SD of each individual     Probe * IND
     message("Zscore Standardizing:")
-    betas <- apply(betas, 1, scale)        
-    rownames(betas) <- colnames(betas)
+    betas <- apply(betas, 2, scale)
+    rownames(betas) <- sample_id
     return(betas)
 }
 
