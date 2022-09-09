90c3f26f5abd90b52e78471d554928edcfa9abfe
Dong Zhao <zd200572@gmail.com>
2022-09-09 10:20:10 +0800

    add Li2018

--- R/availableClock.R
+++ R/availableClock.R
@@ -10,7 +10,7 @@
 availableClock <- function(){
   ## list all available clocks
   all_clock <- c('HannumG2013', 'HorvathS2013', 'LevineM2018', 'ZhangQ2019',
-                 'ShirebyG2020', 'YangZ2016', 'ZhangY2017', 'LuA2019', 
+                 'ShirebyG2020', 'YangZ2016', 'ZhangY2017', 'Li2018', 'LuA2019', 
                  'HorvathS2018', 'DunedinPACE', 'McEwenL2019', 'CBL_specific', 'CBL_common',
                  'Cortex_common')
   all_clock
